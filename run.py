fb240a2c3d86a71a1bef7510933545a0f523a326
Joel Lidin <joellidin@gmail.com>
2020-05-05 16:37:27 +0200

    Added pseudo like code based on the original paper

--- run.py
+++ run.py
@@ -15,4 +15,22 @@ hyperparams = {
 generator = wavegan_generator(64, 1)
 discriminator = wavegan_discriminator(64, 1)
 
+def wgan_loss(generator, discriminator, z, data, batch_size=64):
+    G_z = generator.predict(z)
+    D_G_z = discriminator.predict(G_z)
+    D_x = discriminator.predict(data)
+    G_loss = -tf.math.reduce_mean(D_G_z)
+    D_loss = tf.math.reduce_mean(D_G_z) - tf.math.reduce_mean(D_x)
+
+    alpha = tf.math.random_uniform(shape=[batch_size, 1, 1], minval=0., maxval=1.)
+    differences = G_z - data
+    interpolates = data + (alpha * differences)
+    # with tf.name_scope('D_interp'), tf.variable_scope('D', reuse=True):
+    #   D_interp = WaveGANDiscriminator(interpolates, **args.wavegan_d_kwargs)
+
+    LAMBDA = 10
+    gradients = tf.gradients(D_interp, [interpolates])[0]
+    slopes = tf.math.sqrt(tf.math.reduce_sum(tf.math.square(gradients), reduction_indices=[1, 2]))
+    gradient_penalty = tf.math.reduce_mean((slopes - 1.) ** 2.)
+    D_loss += LAMBDA * gradient_penalty
 
